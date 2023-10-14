a2c0727f2d24943cb13dc06e43f6493729635df3
evanelang <33497277+evanelang@users.noreply.github.com>
2023-10-14 18:49:13 -0400

    Actually working truth table

--- TruthTable.py
+++ TruthTable.py
@@ -4,7 +4,7 @@ def evaluate_boolean_equation(boolean_equation):
     # Implementing a stack to evaluate the boolean equation
     Variables = []
     for char in boolean_equation:
-        if char not in ['(',')','+','*']:
+        if char not in ['(',')','+','*','!']:
             if char not in Variables:
                 Variables.append(char)
     Variables = sorted(Variables)
@@ -20,24 +20,85 @@ def evaluate_boolean_equation(boolean_equation):
         for j in range(columns):
             kmap[i][j] = 0
         
-    left = Variables[:(len(Variables)//2)]
-    top = Variables[(len(Variables)//2):]
+
     terms = boolean_equation.split('+')
     Minterms = []
     for term in terms:
-        leftspot = 0
-        righspot = 0
+        yes = []
+        no = []
         Minterm = 0
         for char in term:
-            if char not in ['(',')','+','*']:
-                if char in left:
-                    leftspot += 2**(len(left) - left.index(char)-1)
-                    Minterm += 2**(len(Variables) - Variables.index(char)-1)
-                elif char in top:
-                    righspot += 2**(len(top) - top.index(char)-1)
-                    Minterm += 2**(len(Variables) - Variables.index(char)-1)
-        print("Left: ", leftspot, "Right: ", righspot)
-        kmap[leftspot][righspot] = 1
+            if char not in ['(',')','+','*','!']:
+                termval = 2**(len(Variables) - Variables.index(char)-1)
+                if term[(term.index(char)-1)] != '!':    
+                    yes.append(termval)
+                    Minterm += termval
+                elif term[(term.index(char)-1)] == '!':
+                    no.append(termval)
+        cyclecount = 0
+        listlengths = len(yes)+len(no)
+        
+        
+        yessave = yes[:]
+        nosave = no[:]
+        for leftspot in range(columns):
+            
+            for rightspot in range(row):
+                yes = yessave[:]
+                no = nosave[:]
+                yestracker = 1
+                notracker = 0
+                spotval = (leftspot*columns) + rightspot
+                for curval in range(listlengths):
+                    
+                    if max(yes, default=0) >= max(no, default=0):
+                        onegreater = 2*max(yes, default=0)
+                        goodnum = yes.pop(0)
+                        
+                        while(spotval >= onegreater):
+                            
+                            for x in range(len(Variables)):
+                                
+                                if spotval >= 2**(len(Variables)-x-1):
+                                    if(spotval >= onegreater):
+                                        spotval = spotval - 2**(len(Variables)-x-1)
+                            
+                                    
+                        
+                        
+                        if goodnum in no:
+                            notracker = 1
+                            
+                        if spotval-goodnum >= 0:
+                            spotval = spotval-goodnum
+                        else:
+                            
+                            yestracker = 0
+                        curval += 1
+                    elif max(yes, default=0) < max(no, default=0):
+                        onegreater = 2*max(no, default=0)
+                        goodnum = no.pop(0)
+                        while(spotval >= onegreater):
+                            for x in range(len(Variables)):
+                                if spotval >= 2**(len(Variables)-x-1):
+                                    spotval = spotval - 2**(len(Variables)-x-1)
+                        
+                        if goodnum in yes:
+                            
+                            notracker = 1
+                        if spotval-goodnum >= 0:
+                            
+                            spotval = spotval-goodnum
+                            notracker = 1
+                        curval += 1
+                    
+
+                if yestracker == 1 and notracker == 0:
+                    print("term: ", term)
+                    print("Hit: ", (leftspot*columns) + rightspot)
+      
+                    kmap[leftspot][rightspot] = 1
+            cyclecount += 1
         Minterms.append(Minterm)
     print(Minterms)
     for h in range(row):
