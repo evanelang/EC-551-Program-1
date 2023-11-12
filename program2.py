dadc55da50f821f763d3d225f7cd991de862782f
mayras22 <146150169+mayras22@users.noreply.github.com>
2023-11-12 16:12:32 -0500

    roadmap of FPGA design

---
+++ program2.py
@@ -0,0 +1,68 @@
+from program1 import program1
+
+class program2 (program1):
+
+    def __init__(self,):
+        pass
+
+    def shared_block_level_controls():
+        #to instantiate all the functions shown bellow
+        pass
+
+    def carrylogic():
+        #arry Logic boosts the logic efficiency and performance of arithmetic functions, including adders, subtracters, 
+        #comparators, binary counters and some wide, cascaded logic functions.
+        pass
+
+    def LUTs():
+        #16x1 rom. acts as 16X1 multiplexer with select and four selectors. i0,i1,i2,i3
+        #gets the ouput from the truth table
+        pass
+    
+
+    def DFF():
+        #D flip flop
+        pass
+
+    def PLLs():
+        #frequency mulipler
+        #synthesize clock frequencies.
+        # there is more functions within this def PLLS. see the manual of IC60
+        pass
+
+    def IO_pin():
+        #The programmable logic associated with an I/O is called a PIO. The individual PIOs are connected to their respective 
+        #sysI/O buffers and pads. The PIOs are placed on the top and bottom of the devices.
+
+        pass
+
+    def SPY_bank():
+        
+        pass
+
+    def global_reset_control():
+        pass
+
+    def Hi_z_control():
+        pass
+
+    def routing():
+        pass
+
+
+def run():
+    pass
+def configure():
+    pass
+def bitstream():
+    #self.bitstream= None
+    #self.files
+    #self.paths
+    pass
+def generate():
+    pass
+
+#test case
+def main():
+    pass
+
