a7b3d3c84ebc434cd222003b90c6b542bb3008ea
evanelang <33497277+evanelang@users.noreply.github.com>
2023-11-12 18:08:18 -0500

    output inputs

--- test_prog.py
+++ test_prog.py
@@ -15,27 +15,44 @@ def processEq(booleanexpr, Vars):
 
 
 if __name__ == '__main__':
-    boolean_equation = input('What is the boolean equation?')
-    Variables = []
+    #boolean_equation = input('What is the boolean equation?')
+
+    runprog = 0
+    NonsingleVars = []
     myoutputs = {}
-    
-    a = boolean_equation.split('=')
-    myout = a[0]
-    for char in a[1]:
-        if char not in ['(',')','+','*','!','&','|', ' ']:
-            if char not in Variables:
-                Variables.append(char)
-    Variables = sorted(Variables)
-    myoutputs[myout] = {}
-    myoutputs[myout]['Variables'] = Variables
-    myoutputs[myout]['Equation'] = a[1]
-    bool2 = sympify(a[1])
-    mytruth = truth_table(bool2, Variables, input=False)
-    
-    newtruth = []
-    x = 0
-    for t in mytruth:
-        newtruth.append(t)
-        x += 1
-    myoutputs[myout]['TruthTable'] = newtruth
+    while(runprog != 1):
+        commandin = input('What would you like to do?')
+        match commandin:
+            case "1":
+                runprog = 0
+                Variables = []
+                boolean_equation = input('What is the boolean equation?') 
+                a = boolean_equation.split('=')
+                myout = a[0]
+                varmaker = a[1]
+                NonsingleVars.append(myout)
+                for subber in NonsingleVars:
+                    if subber in varmaker:
+                        Variables.append(subber)
+                        varmaker = varmaker.replace(subber, ' ')
+                for char in varmaker:
+                    if char not in ['(',')','+','*','!','&','|', ' ']:
+                        if char not in Variables:
+                            Variables.append(char)
+                Variables = sorted(Variables)
+                myoutputs[myout] = {}
+                myoutputs[myout]['Variables'] = Variables
+                myoutputs[myout]['RAWEquation'] = a[1]
+                bool2 = sympify(a[1])
+                mytruth = truth_table(bool2, Variables, input=False)
+                myoutputs[myout]['Equation'] = bool2
+                newtruth = []
+                x = 0
+                for t in mytruth:
+                    newtruth.append(t)
+                    x += 1
+                myoutputs[myout]['TruthTable'] = newtruth
+                #print(myoutputs)
+            case "2":
+                break
     print(myoutputs)
