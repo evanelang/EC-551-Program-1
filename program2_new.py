0472accb8950a9de77d869e1582ad48f783a64ca
evanelang <33497277+evanelang@users.noreply.github.com>
2023-11-14 20:37:32 -0500

    FULL LUT CREATION  AND INTERCONNECTION

--- program2_new.py
+++ program2_new.py
@@ -92,28 +92,86 @@ if __name__ == '__main__':
                 myoutputs[myout]['RAWEquation'] = a[1]
                 varlen = len(Variables)
                 LUTLIST = []
-                while(varlen > 0):
-                    if (varlen - 6) > 0:
-                     LUTLIST.append(myout + "_LUT6")
-                     varlen -= 6
-                    elif (varlen - 4) > 0:
-                     LUTLIST.append(myout + "_LUT6")
-                     varlen -= 4
-                    if (varlen <= 4):
-                     if varlen > 0:
-                        LUTLIST.append(myout + "_LUT4")
-                        varlen = 0
+                #while(varlen > 0):
+                #    if (varlen - 6) > 0:
+                #     LUTLIST.append(myout + "_LUT6")
+                #     varlen -= 6
+                #    elif (varlen - 4) > 0:
+                #     LUTLIST.append(myout + "_LUT6")
+                #     varlen -= 4
+                #    if (varlen <= 4):
+                #     if varlen > 0:
+                #        LUTLIST.append(myout + "_LUT4")
+                #        varlen = 0
                 bool2 = sympify(a[1])
-                bool3 = simplify_logic(bool2)
-                mytruth = truth_table(bool3, Variables, input=False)
-                myoutputs[myout]['LUTS'] = LUTLIST
-                myoutputs[myout]['Equation'] = bool3
+                #bool3 = SOPform(bool2)
+                mytruth = truth_table(bool2, Variables, input=False)
+                myoutputs[myout]['LUTS'] = {}
+                #myoutputs[myout]['Equation'] = bool3
                 newtruth = []
+                minterms = []
                 x = 0
                 for t in mytruth:
-                    newtruth.append(t)
+                    if t == True:
+                        minterms.append(x)
+                        newtruth.append(1)
+                    else:
+                        newtruth.append(0)
                     x += 1
+                bool3 = SOPform(Variables, minterms)
+                myoutputs[myout]['Equation'] = bool3
                 myoutputs[myout]['LUT_LOAD'] = newtruth
+                myoutputs[myout]['Minterms'] = minterms
+                lutsize = int(input("What size should the LUTS be?"))
+                myoutputs[myout]['lutsize'] = lutsize
+                eqsplit = str(bool3).split(' | ')
+                varcount = 0
+                lutcount = 0
+                luteq = ""
+                usedvars = []
+                for term in eqsplit:
+                    varsintermcount = 0
+                    editterm = term
+                    for otherouts in NonsingleVars:
+                        if otherouts in editterm:
+                            if otherouts not in usedvars:
+                                varcount += 1
+                                varsintermcount += 1
+                                usedvars.append(otherouts)
+                            editterm = editterm.replace(otherouts, ' ')
+                            otherlutcount = myoutputs[otherouts]['lutcount']
+                            otherlutsize = myoutputs[otherouts]['lutsize']
+                            term = term.replace(otherouts, otherouts + "_LUT" + str(otherlutsize) + "_" + str(otherlutcount))
+                    for var in Variables:
+                        if var in editterm:
+                            if var not in usedvars:
+                                usedvars.append(var)
+                                varcount += 1
+                                varsintermcount += 1
+                    
+                    if varsintermcount > lutsize:
+                        print("Error: LUT size too small")
+                        exit()
+                    if varcount > lutsize:
+                        myoutputs[myout]['LUTS'][myout + "_LUT" + str(lutsize) + "_" + str(lutcount)] = luteq[:-3]
+                        luteq = myout + "_LUT" + str(lutsize) + "_" + str(lutcount) + " | "
+                        varcount -= (lutsize - 1) 
+                        lutcount += 1
+                    if varcount < lutsize:
+                        luteq += term + " | "
+                    if varcount == lutsize:
+                        luteq += term + " | "
+                        #myoutputs[myout]['LUTS'][myout + "_LUT" + str(lutsize) + "_" + str(lutcount)] = luteq
+                        #luteq = myout + "_LUT" + str(lutsize) + "_" + str(lutcount) + " | "
+                        #lutcount += 1
+                        #varcount = 1
+               
+                myoutputs[myout]['LUTS'][myout + "_LUT" + str(lutsize) + "_" + str(lutcount)] = luteq[:-3]
+                myoutputs[myout]['lutcount'] = lutcount
+                        
+
+
+
                 #print(myoutputs)
             case "12":
                 break
